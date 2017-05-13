589069b488783a4d1268d9d71d75faa99050c750
banganga <alex_5454@list.ru>
2017-05-13 10:26:36 +0500

    call to action form

--- email.php
+++ email.php
@@ -2,8 +2,8 @@
 if($_POST) {
 
 	// Enter the email where you want to receive the message
-	$emailTo = 'email@gmail.com';
-	$subject = 'subject';
+	$emailTo = 'murinskii@gmail.com';
+	$subject = 'New Client';
 
 	$name  = addslashes(trim($_POST['name']));
 	$email = addslashes(trim($_POST['email']));
